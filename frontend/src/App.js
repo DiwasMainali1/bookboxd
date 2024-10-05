af06b3e8f78a1469edc50f9f0c1aa6e4245a8a5c
Diwas Mainali <diwas.sok@gmail.com>
2024-10-05 12:16:59 +1000

    Added and removed files and made the project directory better and more suitable

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-green-600">
+        <h1 className="text-7xl text-center text-blue-800">BookBoxd</h1>
+    </div>
+  );
+}
+
+export default App;
