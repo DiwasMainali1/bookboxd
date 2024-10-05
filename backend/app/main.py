d4011aa33c6bcfe7212ba367dfc62e1306d4907d
Diwas Mainali <diwas.sok@gmail.com>
2024-10-05 12:24:13 +1000

    Added backend

---
+++ backend/app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+   return {"My nama jeff"}
+
+   if __name__ == "__main__":
+       import uvicorn
+       uvicorn.run(app, host="0.0.0.0", port=8000)
+   
