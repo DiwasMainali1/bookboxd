b67d561546b94556f657f2391ccbed7b7376fb58
Diwas Mainali <diwas.sok@gmail.com>
2024-12-06 10:33:24 +1100

    Updated repo

--- frontend/src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="bg-green-600">
-        <h1 className="text-7xl text-center text-blue-800">BookBoxd</h1>
-    </div>
-  );
-}
-
-export default App;
